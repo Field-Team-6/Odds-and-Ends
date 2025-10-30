62f5e7a0ef6fd0468a3ad8686751a70613d7f09d
dbmoore <sttyerase@gmail.com>
2025-10-30 11:05:29 -0400

    Significant changes to connection test to pass tokens as paramenters and fix return values from connection.

--- python_scripts/dbox_connect_test.py
+++ python_scripts/dbox_connect_test.py
@@ -1,23 +1,65 @@
+"""
+THIS SCRIPT TESTS OFFLINE CONNECTION TO DROPBOX USING THE CREDENTIALS CREATED
+BY THE DEVELOPERS APP CONSOLE: https://www.dropbox.com/developers/apps.
+THE CREDENTIALS ARE PROVIDED TO THE SCRIPT AS COMMAND LINE PARAMETERS:
+"""
+
+import argparse
+import sys
 import dropbox
 
-# Use the REFRESH_TOKEN obtained in Step 1
-APP_KEY = ""
-APP_SECRET = ""
-SAVED_REFRESH_TOKEN = ""
+## DEFINE GLOBAL VARIABLES
+APP_KEY = ''
+APP_SECRET = ''
+REFRESH_TOKEN = ''
+idebug = False
 
-# Initialize Dropbox object using the refresh token
-# The SDK handles the token refresh automatically in the background
+## OBTAIN KEY, SECRET, AND REFRESH TOKEN FROM COMMAND LINE ARGUMENTS
+parser = argparse.ArgumentParser(description='dropbox_connect_test.py --appkey appkey --appsecret appsecret --reftoken refresh_token')
+parser.add_argument('-a', '--debug', help='turn on debugging messages', action='store_true')
+parser.add_argument('--appkey', nargs='?', help='App Key provided by Dropbox')
+parser.add_argument('--appsecret', nargs='?', help='App Secret provided by Dropbox')
+parser.add_argument('--reftoken', nargs='?', help='Reference Key provided by  Dropbox'
+                                  '(see https://www.dropbox.com/developers/apps)')
+
+## PARSE COMMAND LINE ARGUMENTS AND VERIFY
+args = parser.parse_args()
+
+if not args.appkey:
+    print('--appkey is mandatory')
+    sys.exit(3)
+else:
+    APP_KEY = args.appkey
+if not args.appsecret:
+    print('--appsecret is mandatory')
+    sys.exit(4)
+else:
+    APP_SECRET = args.appsecret
+if not args.reftoken:
+    print('--reftoken is mandatory')
+    sys.exit(5)
+else:
+    REFRESH_TOKEN = args.reftoken
+if args.debug:
+    idebug = True
+
+if idebug:
+    print('APPKEY: ' +  APP_KEY, ' APPSECRET: ' + APP_SECRET, ' REFRESH_TOKEN: ' + REFRESH_TOKEN)
+
+# INITIALIZE THE DROPBOX OBJECT USING THE KEY, SECRET AND REFRESH TOKEN
+# THE SDK HANDLES THE TOKEN REFRESH AUTOMATICALLY IN THE BACKGROUND
 
 dbx = dropbox.Dropbox(
     app_key=APP_KEY,
     app_secret=APP_SECRET,
-    oauth2_refresh_token=SAVED_REFRESH_TOKEN
+    oauth2_refresh_token=REFRESH_TOKEN
 )
 
+## EVALUATE THE CONNECTION
 try:
     # Example API call
     account_info = dbx.users_get_current_account()
-    print("Successfully connected to Dropbox account: {account_info.name.display_name}")
+    print("Successfully connected to Dropbox account: \n   " ,account_info.name," \n   ",account_info.email)
 
     # You can now proceed with your production API calls,
     # and the SDK will manage the short-lived access tokens for you.
